2c8d24569ad2ae55703f8b12b79618b4c1baf252
Vladimir Taranovskiy <8756136@gmail.com>
2020-05-23 21:58:40 +0300

    Add eslint rules. Delete unnecessary files.

---
+++ src/layouts/Default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page">
+    <div class="page__header">
+      <the-header />
+    </div>
+    <main class="page__main">
+      <div class="page__nav">
+        <the-nav />
+      </div>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader';
+import TheNav from '@/components/TheNav';
+export default {
+  name: 'Default',
+  components: {
+    TheHeader,
+    TheNav,
+  },
+};
+</script>

34cb90db5935484f3454ad46ede5975475906996
Vladimir Taranovskiy <8756136@gmail.com>
2020-05-23 23:35:12 +0300

    Add base styles, variables, mixins

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// styles
+import './assets/styles/main.scss';
+
+// layouts
+import Default from './layouts/Default';
+
+Vue.component('default-layout', Default);
+
 Vue.config.productionTip = false;
 
 new Vue({
